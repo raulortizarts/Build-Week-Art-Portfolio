b5ffa30426f88ce554832a365a720701f974c10c
lesleyfon <lesleybanadzem2017@gmail.com>
2019-07-26 16:43:45 -0700

    Added randoms dates funtion and added more styles to the section divs

--- js/blog.js
+++ js/blog.js
@@ -4,11 +4,17 @@ const asideDiv = document.querySelector(".aside");
 let blogElems = "";
 
 blogObj.forEach((object, index) => {
+    const randomDate = function(){
+        let formatedDate = new Date(+(new Date()) - Math.floor(Math.random()*10000000000))
+        formatedDate = formatedDate.getMonth() + "/" + formatedDate.getDay() +"/" + formatedDate.getFullYear() 
+        return formatedDate
+    
+    }
     blogElems += `
         <div class='section '>
             <header>
-              <h3>Title: ${object.title}</h3>
-              <h5>Author: ${object.author}</h5>
+              <img src = './../images/blogImages/drupal.png'><h3>${object.title}</h3>
+            <h5>On <span>${randomDate()}</span> by ${object.author}</h5>
             </header>
             <div class="entry-content">
               <img src='${object.img}' alt='${object.title}'>
