81bfb5dc297dd1498ad624b2e39bcbdc92bcc3bb
lesleyfon <lesleybanadzem2017@gmail.com>
2019-07-26 15:28:37 -0700

    Styled entry content div so that the image and text are bouth on the same line

--- js/blog.js
+++ js/blog.js
@@ -5,14 +5,14 @@ let blogElems = "";
 
 blogObj.forEach((object, index) => {
     blogElems += `
-        <div class='section'>
+        <div class='section '>
             <header>
               <h3>Title: ${object.title}</h3>
               <h5>Author: ${object.author}</h5>
             </header>
             <div class="entry-content">
               <img src='${object.img}' alt='${object.title}'>
-              <p>Description: ${object.description}</p>
+              <p>${object.description}</p>
             </div>
             
         </div> <hr>
