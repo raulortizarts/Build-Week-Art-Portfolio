eb34903f9a989ec885f5fd7053505f098ebf0088
lesleyfon <lesleybanadzem2017@gmail.com>
2019-07-25 17:26:33 -0700

    added functionality so that when you click login, the the login form will be displayed and when you click signup, the sign up form will be displayed

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
 // get The Modal Div
 const modal = document.getElementById("modalId");
+const signUpForm = document.querySelector('.signUPForm')
+const loginForm = document.querySelector('.loginForm')
+signUpForm.style.display = "none";
+loginForm.style.display = "none";
+console.log(signUpForm)
 
 // get the login/signUp buttons
 const modalOpen = document.querySelectorAll(".modalOpen");
@@ -8,7 +13,14 @@ const closeSpan = document.getElementById("close")
 modalOpen.forEach(btn =>{
     btn.addEventListener("click", function(e){
         e.preventDefault();
+        console.log(e.target.id)
         modal.style.display = "block"
+        if(e.target.id === "login"){
+            console.log("hello")
+            loginForm.style.display = "block";
+        }else if (e.target.id === "signUp"){
+            signUpForm.style.display = "block";
+        }
     })
 })
 
